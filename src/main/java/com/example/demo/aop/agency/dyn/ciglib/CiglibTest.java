5793983911659bc2ef25e6dd73c995ba8c779de6
handa <handa_ly@sina.com>
2020-06-22 17:46:56 +0800

    u

--- src/main/java/com/example/demo/aop/agency/dyn/ciglib/CiglibTest.java
+++ src/main/java/com/example/demo/aop/agency/dyn/ciglib/CiglibTest.java
@@ -1,10 +1,9 @@
 package com.example.demo.aop.agency.dyn.ciglib;
 
-import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.cglib.proxy.Enhancer;
 
-/** @ClassName: CiglibTest @Description: TODO @Author: handa @Date: 2020/7/13 10:56 */
 public class CiglibTest {
+
   public static void main(String[] args) {
       Enhancer enhancer = new Enhancer();
       enhancer.setSuperclass(CiglibAop.class);
