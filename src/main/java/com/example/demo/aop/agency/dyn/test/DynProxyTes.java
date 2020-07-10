f97103c759efd8d674967fea0163a027c21b7c46
handa <handa_ly@sina.com>
2020-07-10 15:01:05 +0800

    动态代理，基于接口

---
+++ src/main/java/com/example/demo/aop/agency/dyn/test/DynProxyTes.java
@@ -0,0 +1,25 @@
+package com.example.demo.aop.agency.dyn.test;
+
+import com.example.demo.aop.Hello;
+import com.example.demo.aop.IHello;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: DynProxyTes
+ * @Description: TODO
+ * @Author: handa
+ * @Date: 2020/7/10 11:14
+ */
+
+public class DynProxyTes {
+
+  public static void main(String[] args) {
+    //
+    List list = new ArrayList();
+    list.add(3);
+//    int hello1 = list.get(0);
+    IHello hello = new HelloInvocationHandler<IHello>().instantProxy(new Hello());
+    hello.sayHello("hello");
+  }
+}
---
+++ src/main/java/com/example/demo/aop/agency/dyn/test/HelloInvocationHandler.java
@@ -0,0 +1,30 @@
+package com.example.demo.aop.agency.dyn.test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @ClassName: HelloInvocationHandler
+ * @Description: TODO
+ * @Author: handa
+ * @Date: 2020/7/10 11:17
+ */
+
+public class HelloInvocationHandler<T> implements InvocationHandler {
+
+    private Object delegate;
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        method.invoke(this.delegate, args);
+        return null;
+    }
+
+    public T instantProxy(T delegate) {
+        this.delegate = delegate;
+        return (T) Proxy.newProxyInstance(this.delegate.getClass().getClassLoader(),
+            this.delegate.getClass().getInterfaces(), this);
+    }
+}
